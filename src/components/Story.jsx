ecc6a451f0456ac97ea55c84b8386d19a6080990
Amr Waheed <amrmohamed877@gmail.com>
2025-02-24 05:36:12 +0200

    finishing the features section

---
+++ src/components/Story.jsx
@@ -0,0 +1,11 @@
+const Story = () => {
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="">the multiversal ip world</p>
+      </div>
+    </section>
+  );
+};
+
+export default Story;
